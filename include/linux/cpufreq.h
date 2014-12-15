e13c3dd715961ccca201c257d4498b0c734e5755
engstk <eng.stk@sapo.pt>
2014-12-15 13:40:54 +0000

    Initial Blu_active import

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -564,6 +564,12 @@ extern struct cpufreq_governor cpufreq_gov_impulse;
 extern struct cpufreq_governor cpufreq_gov_dancedance;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_dancedance)
 
+
+
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BLU_ACTIVE)
+extern struct cpufreq_governor cpufreq_gov_blu_active;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_blu_active)
+
 #endif
 
 /*********************************************************************
